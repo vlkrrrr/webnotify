9f56e08d366728ff8fdf13d79f73d5e0d0076646
yaki-shaft <yakityyakyak@outlook.com>
2023-12-29 20:01:35 +0100

    autowip

--- webnotify/internal/sseclient.go
+++ webnotify/internal/sseclient.go
@@ -5,22 +5,48 @@ import (
 	"bytes"
 	"fmt"
 	"net/http"
+	"time"
 )
 
 func PrintEvents() {
 	c := http.Client{}
 	req, _ := http.NewRequest("GET", "http://localhost:8080/events", nil)
 
-	resp, _ := c.Do(req)
-	scn := bufio.NewScanner(resp.Body)
-	scn.Split(scanEvents)
-	for scn.Scan() {
-		fmt.Println(scn.Text())
-	}
-	if err := scn.Err(); err != nil {
-		fmt.Println("error reading events:", err)
+	getEvents := func() error {
+		resp, err := c.Do(req)
+		if err != nil {
+			return err
+		}
+		scn := bufio.NewScanner(resp.Body)
+		scn.Split(scanEvents)
+
+		for scn.Scan() {
+			fmt.Println(scn.Text())
+		}
+		if err := scn.Err(); err != nil {
+			return err
+		}
+		return nil
 	}
 
+	retry(getEvents)
+}
+
+func retry(f func() error) {
+	bo := 2
+	ty := 3
+	at := 0
+	for at <= ty {
+		err := f()
+		if err != nil {
+			fmt.Println("Error occured: ", err)
+			fmt.Println("Retrying")
+			time.Sleep(time.Duration(at*bo) * time.Second)
+			at++
+		} else {
+			break
+		}
+	}
 }
 
 func scanEvents(data []byte, atEOF bool) (advance int, token []byte, err error) {
