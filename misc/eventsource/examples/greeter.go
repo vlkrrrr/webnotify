b1aa17f6031b3531f2b307b13da31f98ac1d84d1
yaki-shaft <yakityyakyak@outlook.com>
2023-12-29 19:43:21 +0100

    autowip

--- misc/eventsource/examples/greeter.go
+++ misc/eventsource/examples/greeter.go
@@ -1,10 +1,12 @@
 package main
 
 import (
-	"gopkg.in/antage/eventsource.v1"
 	"log"
 	"net/http"
+	"strconv"
 	"time"
+
+	"gopkg.in/antage/eventsource.v1"
 )
 
 func main() {
@@ -12,11 +14,17 @@ func main() {
 	defer es.Close()
 	http.Handle("/", http.FileServer(http.Dir("./public")))
 	http.Handle("/events", es)
+	i := 0
 	go func() {
 		for {
-			es.SendEventMessage("hello", "", "")
+			es.SendEventMessage("hello"+strconv.Itoa(int(time.Now().Unix())), "", "")
 			log.Printf("Hello has been sent (consumers: %d)", es.ConsumersCount())
 			time.Sleep(2 * time.Second)
+			i++
+			if i == 5 {
+				es.Close()
+				break
+			}
 		}
 	}()
 	log.Print("Open URL http://localhost:8080/ in your browser.")
--- webnotify/internal/sseclient.go
+++ webnotify/internal/sseclient.go
@@ -2,6 +2,7 @@ package internal
 
 import (
 	"bufio"
+	"bytes"
 	"fmt"
 	"net/http"
 )
@@ -11,8 +12,36 @@ func PrintEvents() {
 	req, _ := http.NewRequest("GET", "http://localhost:8080/events", nil)
 
 	resp, _ := c.Do(req)
-	r := bufio.NewReader(resp.Body)
-	res, _ := r.ReadString('\n')
+	scn := bufio.NewScanner(resp.Body)
+	scn.Split(scanEvents)
+	for scn.Scan() {
+		fmt.Println(scn.Text())
+	}
+	if err := scn.Err(); err != nil {
+		fmt.Println("error reading events:", err)
+	}
 
-	fmt.Println("event", string(res))
+}
+
+func scanEvents(data []byte, atEOF bool) (advance int, token []byte, err error) {
+	if atEOF && len(data) == 0 {
+		return 0, nil, nil
+	}
+	if i := bytes.Index(data, []byte("\n\n")); i >= 0 {
+		// We have a full newline-terminated line.
+		return i + 1, dropCR(data[0:i]), nil
+	}
+	// If we're at EOF, we have a final, non-terminated line. Return it.
+	if atEOF {
+		return len(data), dropCR(data), nil
+	}
+	// Request more data.
+	return 0, nil, nil
+}
+
+func dropCR(data []byte) []byte {
+	if len(data) > 0 && data[len(data)-1] == '\r' {
+		return data[0 : len(data)-1]
+	}
+	return data
 }
